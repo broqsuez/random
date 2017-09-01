f95e2d224c2ffda166230878a7ef6020cc1e591a
Ishak Imanuel <frontendisawesomee@gmail.com>
2017-09-01 16:18:38 +0700

    add bootstrap4 and font awesome

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import './index';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+import './App.css';
+
 class App extends Component {
   constructor() {
     super();
